f1dd5aa322206a537ce6c51f7558363b890c3025
Javi <javierezpeleta@gmail.com>
2025-02-01 09:40:56 +0100

    adding shadcn + button + primary color

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,10 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
-  return <div>Hello World!!!</div>;
+  return (
+    <div className="p-4">
+      <div>Hello World!!!</div>
+      <Button>Click me!</Button>
+    </div>
+  );
 }
