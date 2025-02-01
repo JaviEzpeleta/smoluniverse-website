dd61b8e725319d6d5691c57b48c3a6d4837f7793
Javi <javierezpeleta@gmail.com>
2025-02-01 10:57:02 +0100

    ok i think i HATE this font because of the line-height.. wtf 😳

--- app/new-clone/page.tsx
+++ app/new-clone/page.tsx
@@ -2,13 +2,17 @@
 
 import Title from "@/components/Title";
 import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
 
 const NewClonePage = () => {
   return (
     <div className="p-6">
       <div className="border-primary/80 border-2 p-4 rounded-md flex flex-col gap-2 items-center justify-center">
         <Title>Create a new Clone</Title>
-        <Button>Create</Button>
+        <div className="flex items-center gap-2">
+          <Input placeholder="twitter handle" className="px-3 !text-lg" />
+          <Button>Create</Button>
+        </div>
       </div>
     </div>
   );
