0933235b29b95b085521f76283eef4701dc02b8f
Javi <javierezpeleta@gmail.com>
2025-02-04 11:52:16 +0100

    signPermit()

--- lib/web3functions.ts
+++ lib/web3functions.ts
@@ -1,4 +1,4 @@
-import { ethers } from "ethers";
+import { Contract, ethers, Wallet } from "ethers";
 import { createWallet, getWalletByHandle } from "./postgres";
 import { postErrorToDiscord, postToDiscord } from "./discord";
 import { cleanHandle } from "./strings";
@@ -27,6 +27,51 @@ export const createAndSaveNewWallet = async (
     return false;
   }
 };
+
+async function signPermit(
+  wallet: Wallet,
+  token: Contract,
+  spender: string,
+  value: bigint,
+  nonce: bigint
+): Promise<string> {
+  if (!wallet.provider) {
+    throw new Error("Wallet provider not found");
+  }
+  // Obtener el DOMAIN_SEPARATOR del contrato token
+  const domain = {
+    name: await token.name(),
+    version: "1",
+    chainId: await wallet.provider.getNetwork().then((n) => n.chainId),
+    verifyingContract: token.address,
+  };
+
+  // Use MaxUint256 from ethers
+  const maxDeadline = ethers.MaxUint256;
+
+  const message = {
+    owner: wallet.address,
+    spender: spender,
+    value: value,
+    nonce: nonce,
+    deadline: maxDeadline,
+  };
+
+  const types = {
+    Permit: [
+      { name: "owner", type: "address" },
+      { name: "spender", type: "address" },
+      { name: "value", type: "uint256" },
+      { name: "nonce", type: "uint256" },
+      { name: "deadline", type: "uint256" },
+    ],
+  };
+
+  // Firmar el mensaje usando EIP-712
+  const signature = await wallet._signTypedData(domain, types, message);
+
+  return signature;
+}
 
 export const sendInitialFundsToWallet = async (address: string) => {
   const deployerWalletPrivateKey = process.env.DEPLOYER_WALLET_PRIVATE_KEY;
