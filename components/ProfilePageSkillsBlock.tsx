c00398aea5a9ca689e917be469c9938219cd07c3
Javi <javierezpeleta@gmail.com>
2025-02-05 17:07:31 +0100

    finally: better-looking skills, with the bar indicator

--- components/ProfilePageSkillsBlock.tsx
+++ components/ProfilePageSkillsBlock.tsx
@@ -4,6 +4,7 @@ import ToolTipped from "./ToolTipped";
 import MiniTitle from "./MiniTitle";
 import BlurryEntrance from "./BlurryEntrance";
 import NumberFlow from "@number-flow/react";
+import { motion } from "framer-motion";
 
 const ProfilePageSkillsBlock = ({ skills }: { skills: any }) => {
   return (
@@ -26,19 +27,52 @@ const ProfilePageSkillsBlock = ({ skills }: { skills: any }) => {
               description: string;
             },
             index: number
-          ) => (
-            <ToolTipped text={description} key={index}>
-              <BlurryEntrance delay={index * 0.055 + 0.12}>
-                <div className="bg-zinc-800 rounded-lg p-2 pb-1 flex items-center gap-2 justify-between">
-                  <div className="flex items-center gap-2">
-                    <div>{emoji}</div>
-                    <div>{name}</div>
-                  </div>
-                  <NumberFlow value={Number(level)} />
+          ) => {
+            let gradientColor =
+              "bg-gradient-to-r from-smolGreen/40 to-smolGreen";
+            if (Number(level) < 80) {
+              gradientColor =
+                "bg-gradient-to-r from-yellow-400/40 to-yellow-400";
+            }
+            if (Number(level) < 60) {
+              gradientColor = "bg-gradient-to-r from-amber-400/40 to-amber-400";
+            }
+            if (Number(level) < 40) {
+              gradientColor =
+                "bg-gradient-to-r from-orange-400/40 to-orange-400";
+            }
+            if (Number(level) < 20) {
+              gradientColor = "bg-gradient-to-r from-red-400/40 to-red-400";
+            }
+            return (
+              <ToolTipped text={description} key={index}>
+                <div className="relative overflow-hidden rounded-md">
+                  <BlurryEntrance delay={index * 0.055 + 0.12}>
+                    <div className="bg-zinc-800 p-2 pb-1 flex items-center gap-2 justify-between">
+                      <div className="flex items-center gap-2">
+                        <div>{emoji}</div>
+                        <div>{name}</div>
+                      </div>
+                      <NumberFlow value={Number(level)} />
+                    </div>
+                    <div className="absolute rounded-xl bottom-0 w-full pointer-events-none h-0.5 bg-zinc-950">
+                      <motion.div
+                        className={`h-0.5 rounded-xl ${gradientColor}`}
+                        initial={{ width: 0 }}
+                        animate={{
+                          width: `${level}%`,
+                        }}
+                        transition={{
+                          duration: 1.2,
+                          ease: "easeOut",
+                        }}
+                      ></motion.div>
+                    </div>
+                  </BlurryEntrance>
                 </div>
-              </BlurryEntrance>
-            </ToolTipped>
-          )
+              </ToolTipped>
+            );
+          }
         )}
       </div>
     </div>
